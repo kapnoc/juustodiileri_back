e586b03de4057b9f5f8d16095db4b960421329cb
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-12-10 11:57:56 +0100

    ALLOWED_HOSTS

--- juustodiileri_back/settings_prod.py
+++ juustodiileri_back/settings_prod.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["juustodiileri.kapno.cc"]
+ALLOWED_HOSTS = ["juustodiileri.kapno.cc", ".kapno.cc"]
 
 
 # Application definition

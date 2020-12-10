8a6e8580a5328fbdbfc83c62e2496a9beff0fbc3
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-12-10 10:48:11 +0100

    gcp storage + thumbnail config

--- juustodiileri_back/settings_prod.py
+++ juustodiileri_back/settings_prod.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l2)!gbsi0o@kyp))l%np+6xkwx8k=4lchx_r%*_0v_zkx(g^uc'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
 
 
 # Application definition
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'juustodiileri_back.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {}
 }
+DATABASES["default"] = env.db("DATABASE_URL")
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
 
 # Password validation
@@ -135,13 +135,14 @@ STATICFILES_DIRS = [
     BASE_DIR / 'juustodiileri_back/static'
 ]
 
+
 # Google Cloud Storage
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = 'juustodiileri-dev'
+GS_BUCKET_NAME = 'juustodiileri-prod'
 GS_PROJECT_ID = 'juustodiileri'
 GS_FILE_OVERWRITE = False
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    "./credentials.json"
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
+    json.loads(env("GS_CREDENTIALS_JSON"))
 )
 
 
